b0a93b0362f706bdb124a472ea1376149823281c
Hanielxlopezcornelis <hanielxuanlc@gmail.com>
2025-12-03 14:28:13 -0300

    New Project
    Creating a portfolio

---
+++ my-portfolio/src/components/header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <div className="Header">
+            <title>Haniel Xuan López Cornelis | Software Developer</title>
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>   
+    );
+}
